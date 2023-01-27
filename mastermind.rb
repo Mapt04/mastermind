b70cdfa47b0c3f4a95d563b8b65a1878b197695a
Mapt04 <maptib04@gmail.com>
2023-01-26 19:40:16 -0500

    Add play loop and fix Game.check_guess()

--- mastermind.rb
+++ mastermind.rb
@@ -3,25 +3,55 @@ class Game
         @code_breaker = Player.new(self)
         @guesses = []
         @code = @code_breaker.create_code
+        @turn_number = 1
+    end
+
+    def play
+        puts "Welcome to MasterMind!"
+        puts "The code is #{@code}"
+        loop do
+            puts "\n\nTurn #{@turn_number}/12"
+            print_board
+            guess = @code_breaker.guess
+            @guesses.push(guess)
+            clue = check_guess(guess)
+            if check_win(clue)
+                puts "\nThe code breaker won! The code was #{@code}"
+                return
+            end
+            if @turn_number == 12
+                puts "\nThe code maker won! The code was #{@code}"
+            end
+            @turn_number += 1
+        end
     end
 
     def print_board
+        puts
         @guesses.each {|guess| puts "#{guess} | #{check_guess(guess)}"}
+        puts
     end
 
     def check_guess(guess)
-        left_to_guess = @code
+        left_to_guess = @code.clone
+        left_to_check = guess.clone
         clue = ""
-        guess.each_char.with_index do |x, index|
+        left_to_check.each_char.with_index do |x, index|
             if left_to_guess[index] == x
                 clue += "*"
                 left_to_guess[index] = "-"
+                left_to_check[index] = "-"
+                p left_to_guess
             end
         end
-        guess.each_char do |x|
-            if left_to_guess.include? x
-                clue += "?"
-                left_to_guess.sub!(x, "-")
+        left_to_check.each_char.with_index do |x, index|
+            unless x == "-"
+                if left_to_guess.include? x
+                    clue += "?"
+                    left_to_guess.sub!(x, "-")
+                    left_to_check[index] = "-"
+                    p left_to_guess
+                end
             end
         end
         left_to_guess.each_char do |x|
@@ -57,3 +87,5 @@ class Player
         end
     end
 end
+
+Game.new.play
